45266ea2c8ec8d7c2e8fca71b0ae137ea9c83371
Christopher Homberger <christopher.homberger@web.de>
2019-11-04 23:32:59 +0100

    Set minsdk to 10.10 via environment

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$PWD/depot_tools
+export FORCE_MAC_SDK_MIN=10.10
 curl -L https://github.com/aktau/github-release/releases/download/v0.7.2/darwin-amd64-github-release.tar.bz2 --output github-release.tar.bz2
 tar -xf github-release.tar.bz2
 git clone https://chromium.googlesource.com/angle/angle
