2cf0aeced326ef31453053e6c3f2b0b44ac7eea2
Christopher Homberger <christopher.homberger@web.de>
2019-11-06 23:29:23 +0100

    checkout after cd

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ export PATH=$PATH:$PWD/depot_tools
 export FORCE_MAC_SDK_MIN=10.10
 git clone -n https://chromium.googlesource.com/angle/angle
 # Try older commit Could be nearly the last working Version for 32 bit MacOs <= 10.14
-git checkout ba65c156cf76e73ddbb405a26f299cf079af0f92
 cd angle
+git checkout ba65c156cf76e73ddbb405a26f299cf079af0f92
 python scripts/bootstrap.py
 gclient sync
 git checkout master
