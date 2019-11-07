85dacaecac41884eff451f9c282eb84f7285a686
Christopher Homberger <christopher.homberger@web.de>
2019-11-07 16:15:39 +0100

    Initial Working Version

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 export PATH=$PATH:$PWD/depot_tools
-export FORCE_MAC_SDK_MIN=10.10
 git clone -n https://chromium.googlesource.com/angle/angle
-# Try older commit Could be nearly the last working Version for 32 bit MacOs <= 10.14
 cd angle
-git checkout ba65c156cf76e73ddbb405a26f299cf079af0f92
+# Try older commit could be nearly the last working Version for 32 bit MacOs <= 10.14 without patches
+export BUILD_COMMIT=${BUILD_COMMIT:-076fae01016528aa58a8af7ea817a922d297cc53}
+git checkout ${BUILD_COMMIT}
 python scripts/bootstrap.py
 gclient sync
-git checkout ba65c156cf76e73ddbb405a26f299cf079af0f92
-gn gen out/Release --args='target_cpu="x86" is_debug=false'
+git checkout ${BUILD_COMMIT}
+gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.10"'
 autoninja -C out/Release
 cd ..
 mkdir ./artifacts
-cp angle/out/Release/*.dylib ./artifacts/
-cp angle/out/Release/*.a ./artifacts/
+cp angle/out/Release/*.dylib ./artifacts/ > /dev/null || :
+cp angle/out/Release/*.a ./artifacts/ > /dev/null || :
