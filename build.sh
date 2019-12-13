ebaee52acd6e1d79270ad26cfb7e98af311ef301
Christopher Homberger <christopher.homberger@web.de>
2019-12-13 14:42:20 +0100

    Build only libEGL and libGLESv2

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ cd angle
 python scripts/bootstrap.py
 gclient sync
 gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.10" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
-autoninja -C out/Release
+autoninja -C out/Release libEGL libGLESv2
 cd ..
 mkdir ./artifacts
 cp angle/out/Release/*.dylib ./artifacts/ > /dev/null || :
