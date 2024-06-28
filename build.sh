b680ae2fd0f5c6ffdb9bb5c84576a88a95910feb
Alex Malyutin <alex.malyutin.dev@gmail.com>
2024-06-28 23:37:25 +0400

    Update build script.

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git --depth=1
 export PATH=$PATH:$PWD/depot_tools
-git clone https://github.com/minecraft-linux/angle.git
+git clone https://github.com/google/angle.git --depth=1
 cd angle
-python scripts/bootstrap.py
-gclient sync
-gn gen out/Release --args='target_cpu="x86" is_debug=false mac_sdk_min="10.10" angle_enable_metal=false angle_enable_swiftshader=false angle_enable_vulkan=false'
+python3 scripts/bootstrap.py
+gclient sync --shallow --no-history -D -R
+gn gen out/Release --args='target_cpu="arm64" is_debug=false mac_sdk_min="10.10" angle_enable_metal=true angle_enable_glsl=true angle_enable_swiftshader=false angle_enable_vulkan=false'
 autoninja -C out/Release libEGL libGLESv2
 cd ..
 mkdir ./artifacts
